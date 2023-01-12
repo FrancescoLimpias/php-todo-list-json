a5339ef289d67d72d48e42f77799cfb7a3c2ea40
Francesco Limpias <francescolimpias@gmail.com>
2023-01-12 16:06:43 +0100

    Added base GET listeners to list.php microservice

--- htdocs/api/list.php
+++ htdocs/api/list.php
@@ -1,3 +1,30 @@
 <?php
 
-session_start();
+// retrieve session data
+require __DIR__ . "/../php/config.php";
+
+// check for auth
+if (!$USER) {
+
+    // request not authorized
+    echo "ERROR 403: authentication has not been provided";
+    echo "<br>visit localhost for the API reference";
+    die();
+}
+// user is logged => proceed
+
+// switch on request type
+// DEBUG: change GET to SET as soon as front has passed necessary api tests
+if (isset($_GET)) {
+    switch ($_GET["request"]) {
+        case "list":
+            echo "list";
+            break;
+        case "set":
+            echo "set";
+            break;
+        case "del":
+            echo "del";
+            break;
+    }
+}
